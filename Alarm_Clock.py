40f9b70b9cb2afd72bb5ee556905bbc9d1f26a54
GuillaumeCode17 <coding72d@gmail.com>
2022-09-02 14:47:32 -0400

    Alarm_ClockV1

--- Alarm_Clock.py
+++ Alarm_Clock.py
@@ -14,8 +14,8 @@ root.config(background="black")
 root.resizable(False, False)
 width = root.winfo_screenwidth()
 height = root.winfo_screenheight()
-LINKED_STARTED = True
 mixer.init()
+Current_Alarm_State = True
 
 
 def clock():
@@ -31,43 +31,40 @@ def th():
 
 
 def a():
+    Current_Alarm_State = False
+    AlarmTime = ""
     a = AlarmSet.get()
     if a == "":
         msg = messagebox.showerror(
             'Invalid data', 'Please enter valid time | Example ( 15:45 )')
     else:
-        Alarmtime = a
+        AlarmTime = a
         CurrentTime = time.strftime("%H:%M")
         SetTime.config(bg="green")
+        AlarmSetLabel.config(text="Time's Set to : \n" +
+                             AlarmTime+" ⏰", fg="green")
+        Current_Alarm_State = True
 
-        while Alarmtime != CurrentTime:
-            CurrentTime = time.strftime("%H:%M")
-
-        if Alarmtime == CurrentTime:
-            mixer.music.load('Bird01.mp3')
-            mixer.music.play()
-            msg = messagebox.showinfo('It is time', f'{amsg.get()}')
-            if msg == 'ok':
-                mixer.music.stop()
-                SetTime.config(bg="red")
-
+        if AlarmTime != CurrentTime:
+            while Current_Alarm_State:
+                CurrentTime = time.strftime("%H:%M")
+                if AlarmTime == CurrentTime:
+                    mixer.music.load("./Alarm01.mp3")
+                    mixer.music.play(loops=2)
+                    msg = messagebox.showinfo('It is time', f'{amsg.get()}')
+                    if msg == 'ok':
+                        mixer.music.stop()
+                        SetTime.config(bg="red")
+                        AlarmSetLabel.config(text="")
+                        Current_Alarm_State = False
 
 # Labels Zone ==========================================================|
+
 
 # Top Image
 first_img = PhotoImage(file="./Images/Alarm01.png")
 my_img = Label(root, image=first_img, width=800, height=300, bg="black")
 my_img.grid(row=0, column=0, pady=(10, 0), padx=5)
-
-
-# btn to set alarm time
-SetTime = Button(root, width=10, bg="red", fg="white",
-                 text="Set Time", command=th, padx=10, pady=5, cursor="hand2")
-SetTime.grid(row=5, column=0, padx=10, pady=10)
-
-AlarmSet = Entry(root, insertbackground="white", font=("comic sans", 20),
-                 width=5, bg="black", fg="white", border=10)
-AlarmSet.grid(row=6, column=0, padx=10, pady=10)
 
 
 try:  # Pop up confirmation alarm warning
@@ -80,6 +77,27 @@ try:  # Pop up confirmation alarm warning
     amsg.grid(row=3, column=0, columnspan=2, padx=10, pady=5)
 except:
     pass
+
+
+# btn to set alarm time
+SetTime = Button(root, width=10, bg="red", fg="white",
+                 text="Set Time", command=th, padx=10, pady=5, cursor="hand2")
+SetTime.grid(row=5, column=0, padx=10, pady=10)
+
+
+AlarmSetLabelMsg = Label(root, text="Enter Alarm's Time \n Format e.g. (15:45) = 3h00PM",
+                         fg="white", font=("Times", 20), bg="black")
+AlarmSetLabelMsg.grid(row=6, column=0, pady=10)
+
+AlarmSet = Entry(root, insertbackground="white", font=("comic sans", 20),
+                 width=5, bg="black", fg="white", border=10)
+AlarmSet.grid(row=7, column=0, padx=10, pady=10)
+
+
+AlarmSetLabel = Label(root, text="", bg="black",
+                      fg="white", font=("Times", 20))
+AlarmSetLabel.grid(row=8, column=0, pady=10)
+
 
 try:  # Current Time Label and # developer Label
     # Current Time Label
